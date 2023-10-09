5a5df1ae2b9f05c3f210bc5eb05f8a1ca0e3e9d8
Samuel Eras <sa.eras@gmx.de>
2023-10-09 17:17:25 +0200

    basic setup

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const dayjs = require("dayjs");
+const { default: MicroModal } = require("micromodal");
+
+let myDate = new Date();
+
+console.log(myDate);
+
+
+//First Setup git in VS, then create the timezoneapp as public repo with days and micromodal as described in the fullstack roadmap. Search for the package documentaries on npmjs.com
+
+
+dayjs
+
+MicroModal
